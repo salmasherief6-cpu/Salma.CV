6b2c5b51503aae163440bd5bd1cb2737fbcf68d5
salmasherief6-cpu <salmasherief6@gmail.com>
2026-06-23 18:24:10 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const cards = document.querySelectorAll(".card");
+
+cards.forEach(card => {
+    card.addEventListener("mouseenter", () => {
+        card.style.boxShadow = "0 0 20px #00d4ff";
+    });
+
+    card.addEventListener("mouseleave", () => {
+        card.style.boxShadow = "none";
+    });
+});
